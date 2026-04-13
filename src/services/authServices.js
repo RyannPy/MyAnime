7562d30ffb1dac37396b5a480ec179e9de0b4418
Ryan <ryanbayu1233@gmail.com>
2026-04-13 16:27:58 +0700

    feat: add authServices and Auth logic

---
+++ src/services/authServices.js
@@ -0,0 +1,28 @@
+import { supabase } from "./supabase";
+
+// REGISTER
+export const signUp = async (email, password) => {
+    return await supabase.auth.signUp({
+        email,
+        password
+    });
+};
+
+// LOGIN
+export const signIn = async (email, password) => {
+    return await supabase.auth.signIn({
+        email,
+        password
+    });
+};
+
+// LOGOUT
+export const signOut = async () => {
+    return await supabase.auth.signOut();
+};
+
+// GET USER
+export const getCurrentUser = async () => {
+    const { data } = await supabase.auth.getUser();
+    return data?.user;
+}
